d6a19c373a047985319260d007512ef902f2cde0
GUL-Bao <BaoPham92@yahoo.com>
2019-10-30 15:22:42 -0400

    test sql db working

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,38 @@
 // * SETTING PKGS
 const express = require('express');
 const app = express();
-const morgan = require('morgan')
+const morgan = require('morgan');
+const mysql = require('mysql');
 
+// ? PORT TO SERVE
 const port = 5000;
 
+// * Methods
+const jsonModifier = param => JSON.parse(JSON.stringify(param))
+
 // * MIDDLEWARE
-app.use([morgan('combined'), express.json()])
+const configuration = [morgan('combined'), express.json()]
+app.use(configuration)
 
 // * ROUTES
 app.get('/', (req, res) => {
     res.send('Root directory || Welcome to Home Page')
+})
+
+// * SAMPLE ROUTE TESTING MYSQL CLI DB
+app.get('/users', async (req, res) => {
+    console.log(`FETCHING USER DATA`)
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'test'
+    });
+
+    await connection.query("SELECT * FROM users", (err, rows, fields) => {
+        res.json(jsonModifier(rows))
+    })
 })
 
 app.listen(port, () => console.log(`App is listening on ${port}`))
