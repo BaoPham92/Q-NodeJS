7059cfb4e9f563f23a412806dd57c812256b4550
GUL-Bao <BaoPham92@yahoo.com>
2019-10-31 00:23:51 -0400

    Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom'; 
-
-//component imports
-import './App.css';
-import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
      <h1>People's Republic of West Korea</h1>
-     <Login />
-     <Switch>
-        <Route />
-        <Route />
-        <Route />
-     </Switch>
-
-
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import AppRouter from './Routes/AppRouter';
 import './index.css';
+import './App.css';
 import App from './App';
 
+const jsx = (
+    <AppRouter>
+        <App />
+    </AppRouter>
+)
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(jsx, document.getElementById('root'));
 
 
 
