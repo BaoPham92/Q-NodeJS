f3c1ebaaf7fbc5fc0adc7875d4c77461b04bff2b
mxxt1 <matt@dustcloudlabs.com>
2019-10-30 22:31:06 -0400

    login components in place

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch, Redirect } from 'react-router-dom'; 
+
+//component imports
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
      <h1>People's Republic of West Korea</h1>
+     <Login />
+     <Switch>
+        <Route />
+        <Route />
+        <Route />
+     </Switch>
+
+
     </div>
   );
 }
 
 export default App;
+
+
+
---
+++ frontend/src/components/HomeWrapper.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from './Login.js';
+
+
+
+const HomeWrapper = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default HomeWrapper
