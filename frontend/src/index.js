46359d982a1a9309e645a50b3408a123d47d00c6
GUL-Bao <BaoPham92@yahoo.com>
2019-10-31 00:25:56 -0400

    Removed App.js & CSS

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './Routes/AppRouter';
 import './index.css';
-import './App.css';
-import App from './App';
 
-const jsx = (
-    <AppRouter>
-        <App />
-    </AppRouter>
-)
+const jsx = (<AppRouter />)
 
 ReactDOM.render(jsx, document.getElementById('root'));
 
