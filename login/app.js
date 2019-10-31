f3dadc8e948e75df2fb5e3c669c37624df22bcc4
HaZeust <hazeust@gmail.com>
2019-10-30 23:04:27 -0400

    Login system Commit
    r/youseeingthisshit/

---
+++ login/app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var login =require('./routes/loginroutes');
+var path = require('path');
+var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+
+
+var index = require('./routes/index');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+//404 type stuff
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Request and error handler, as seen on Sketchboard
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
