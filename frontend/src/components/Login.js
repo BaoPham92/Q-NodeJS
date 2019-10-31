33a0eaefcd838620a0155829d330a7a89c1a4975
GUL-Bao <BaoPham92@yahoo.com>
2019-10-31 15:32:42 -0400

    Form for sign up & login component

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Link from 'react-router-dom';
+import Form from './utils/Member/Form';
 
 const Login = (props) => {
     const [user, setUser] = useState({
@@ -27,14 +28,7 @@ const Login = (props) => {
     }
 
     return (
-
-        <form onSubmit={handleSubmit}>
-            <h1>Login</h1>
-            <input onChange={handleChange} type='text' name='name' placeholder='Name' value={user.name} />
-            <input onChange={handleChange} type='password' name='password' placeholder='Password' value={user.password} />
-            <input onChange={handleChange} type='email' name='email' placeholder='Email' value={user.email} />
-            <button type='submit'>Submit</button>
-        </form>
+        <Form handleChange={handleChange} handleSubmit={handleSubmit}/>
     )
 }
 
