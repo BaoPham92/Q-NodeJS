7539df1f2deba50f5d672946ebaa20862a6568a0
Tanner Glazier <taglazier15@users.noreply.github.com>
2019-11-01 19:32:51 -0400

    Delete python_install.ps1

--- scripts/powershell/python_install.ps1
+++
@@ -1,5 +0,0 @@
-#Installs Chocolatey (This is used to install python)
-Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-#install python
-choco install -y python3
