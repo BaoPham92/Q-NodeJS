10dee916085ba1979de95b2e15a40f9305f41ffd
Tanner Glazier <tanner78543416@gmail.com>
2019-11-01 20:36:55 -0400

    creates sql install and update password in settings.json in sql

---
+++ scripts/sql_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#sql server install
+sudo apt-get -y install mysql-server
+
+#start sql server
+sudo service mysql start
+
+#This is code for testing can be taken out or left alone
+#This is to check to see if the sql server is running
+sudo service mysql status
+
+#create credentials and create database info
+mysql --host=localhost --user=root --password=itHeYAlOM mysql
+mysql -h localhost --user=root --password=itHeYAlOM -e "create database info;"
+mysql --host=localhost --user=root --password=itHeYAlOM info
+
+#loads the sql table into the database
+mysql info < cd ~/sql/info_members.sql
