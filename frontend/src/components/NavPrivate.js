91bf3f4b3426eb015b5ad7d4a892d93eade389bc
mxxt1 <matt@dustcloudlabs.com>
2019-11-03 14:55:07 -0500

    updated privatenav

--- frontend/src/components/NavPrivate.js
+++ frontend/src/components/NavPrivate.js
@@ -8,6 +8,13 @@ const NavPrivate = (props) => {
         return (
             <>
                 <style.ul>
+                    <li>
+                        <div>
+                            <img src = {require('./landing/assets/logo.jpg')} alt = {'logo'} />
+                            <p>Fédération Internationale Des
+                            Sanctionnés Tyrans </p>
+                        </div>
+                    </li>
                     <li><NavLink to="/">Home</NavLink></li>
                     <li><NavLink to="/news">News</NavLink></li>
                     <li><NavLink to="/members">Members</NavLink></li>
