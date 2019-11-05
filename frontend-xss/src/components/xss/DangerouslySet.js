ad48f45fffee48fd8ffed269f40e72ba3290fe81
mxxt1 <matt@dustcloudlabs.com>
2019-11-05 17:57:03 -0500

    structured out xss vulnerability search input  --> dangerouslySetInnerHtml --> render input to DOM

---
+++ frontend-xss/src/components/xss/DangerouslySet.js
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const DangerouslySet = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DangerouslySet
+
+
+
+
+
+// function MyComponent() {
+//     const title = response.from.backend.title;
+//     return <div dangerouslySetInnerHTML={{__html: title}} />; // BAD - potential XSS attack could happened
+//   }
+
+
+// 1) have search bar
+// 2) Info entered into the search bar is rendered to screen <---- vector of attack
+// 3)
+
+import React, {useState} from 'react'
+import queryString from 'query-string';
+
+const DangerouslySet = (props) => {
+
+const [query, setQuery] = useState({
+    string:''
+});
+
+    const handleQuery = e => {
+        e.preventDefault();
+        setQuery(e.target.value);
+    }
+
+    const handleQuerySubmit = e => {
+
+    }
+
+    return (
+        <div>
+            <input 
+                type="search"
+                value={query.string}
+                onChange={handleQuery}
+                placeholder='Search Members'
+            />
+            <button>Search</button>
+            <div 
+            dangerouslySetInnerHTML={
+                {__html: `<div>${query.string}</div>`}
+            }
+            
+            />   
+        </div>
+    )
+}
+
+export default DangerouslySet
