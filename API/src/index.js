8c303b7f60b63172917c0aef5f40331438c9091d
Bao Pham <sks@new-host-3.home>
2019-11-11 22:33:58 -0500

    fixed cors issue, tested a working fetch API that will work for the XSS

--- API/src/index.js
+++ API/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const router = require('./routes/dictators.js');
-// const cors = require('cors');
+const cors = require('cors');
 
 app.set('port', process.env.PORT || 5000);
 app.use([express.json(), cors()]);
@@ -11,7 +11,7 @@ app.use('/api', router);
 
 // TODO: CLEAN UP THIS REQ LATER. THIS IS HERE USED FOR THE XSS FROM XSS FRONT-END
 app.get('/', async (req, res) => {
-  return await res.status(200).json({ message: res })
+  return await res.status(200).json({ message: 'endpoint reached!' })
 })
 
 app.listen(app.get('port'), () => {
