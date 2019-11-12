67b1e97a5eca7ccb45519c2031e7ba262ca797ad
Tanner Glazier <tanner78543416@gmail.com>
2019-11-11 20:17:48 -0500

    update

--- scripts/sqli_startup.sh
+++ scripts/sqli_startup.sh
@@ -2,11 +2,11 @@
 gnome-terminal
 cd Q-NodeJS/login
 npm i
-gnome-terminal -e npm run start
+gnome-terminal -- npm run start
 
 
 cd .. 
 cd frontend-sqli
-gnome-terminal npm start
+gnome-terminal -- npm start
 
-gnome-terminal firefox 127.0.0.1/5000
+gnome-terminal -- firefox 127.0.0.1/5000
